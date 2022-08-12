1a61c8bb085fdd823cdc137f549caced5ca0dedc
Nicholas <nicholasjoven@gmail.com>
2022-08-12 14:24:40 -0400

    commit tree 25f28dd602068548aa4d9afde9d663e774b6a072

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    check_errors
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_larger_than_0?
+    a > 0 && b > 0 && c > 0
+  end
+
+  def obeys_inequality?
+    a + b > c && a + c > b && b + c > a 
+  end
+
+  def check_errors
+    raise TriangleError unless sides_larger_than_0? && obeys_inequality?
+  end
+
+    
+
+
+  class TriangleError < StandardError
+  end
 end
